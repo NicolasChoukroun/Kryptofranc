ff22e89e12e00eafaac59857e5f1443fe59923ea
William Choukroun <williamchoukroun1997@gmail.com>
2018-09-03 22:36:25 +0700

    Installer for Unix and Windows are fully functional

--- Bitfranc_installer_Unix/script_installer_Unix.sh
+++ Bitfranc_installer_Unix/script_installer_Unix.sh
@@ -1,29 +1,33 @@
+#!/bin/bash -xv
 sudo apt-get --assume-yes update
+sudo apt-get --assume-yes upgrade
 sudo apt-get --assume-yes install git
-mkdir src
+sudo apt-get --assume-yes install autoconf libtool pkg-config libboost-all-dev libssl-dev libprotobuf-dev protobuf-compiler libevent-dev libqt4-dev libcanberra-gtk-module
 #git clone https://github.com/bitcoin/bitcoin.git
-cp -r ../bitcoin src/
-cd src
-rm -rf bitcoin/src/qt/locale
-cp -R ../locale bitcoin/src/qt/
-cp ../bitcoin-config.h bitcoin/src/config/
-cp ../bitcoin.png bitcoin/src/qt/res/icons/bitcoin.png
-cp ../bitcoin.ico bitcoin/src/qt/res/icons/bitcoin.ico
+
+INSTALL_PWD_TMP=$PWD
+sudo cp -r ../bitcoin ~/
+sudo rm -rf ~/bitcoin/src/qt/locale
+sudo cp -R locale ~/bitcoin/src/qt/
+sudo cp bitcoin-config.h ~/bitcoin/src/config/
+sudo cp bitcoin.png ~/bitcoin/src/qt/res/icons/bitcoin.png
+sudo cp bitcoin.ico ~/bitcoin/src/qt/res/icons/bitcoin.ico
 sudo apt-get --assume-yes install build-essential
-wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
-tar -xvf db-4.8.30.NC.tar.gz
-rm -rf db-4.8.30.NC.tar.gz
+sudo apt-get --assume-yes install qt5-default qttools5-dev-tools
+
+sudo mkdir ~/bitcoin/db4/
+cd ~/bitcoin/db4
+sudo wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+sudo tar -xzvf db-4.8.30.NC.tar.gz
+sudo rm -rf db-4.8.30.NC.tar.gz
 cd db-4.8.30.NC/build_unix
-mkdir -p build
-BDB_PREFIX=$(pwd)/build
-sudo ../dist/configure --disable-shared --enable-cxx --with-pic --prefix=$BDB_PREFIX
+sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=~/bitcoin/db4/
 sudo make install
-cd ../..
-sudo apt-get --assume-yes install autoconf libtool pkg-config libboost-all-dev libssl-dev libprotobu\
-f-dev protobuf-compiler libevent-dev libqt4-dev libcanberra-gtk-module
-cd bitcoin
-git checkout v0.16.0
+
+cd ~/bitcoin
+sudo apt-get --assume-yes install autoconf libtool pkg-config libboost-all-dev libssl-dev libprotobuf-dev protobuf-compiler libevent-dev libqt4-dev libcanberra-gtk-module
+
 sudo ./autogen.sh
-sudo ./configure CPPFLAGS="-I${BDB_PREFIX}/include/ -O2" LDFLAGS="-L${BDB_PREFIX}/lib/" --with-gui
-cp ../bitcoin_config.h build_msvc/bitcoin_config.h
+sudo ./configure LDFLAGS="-L~/bitcoin/db4/lib/" CPPFLAGS="-I~/bitcoin/db4/include/"
+sudo cp $INSTALL_PWD_TMP/bitcoin_config.h ~/bitcoin/build_msvc/bitcoin_config.h
 sudo make
--- Bitfranc_installer_Windows/script-Windows.sh
+++ Bitfranc_installer_Windows/script-Windows.sh
@@ -1,30 +1,36 @@
+#!/bin/bash -xv
 sudo apt-get --assume-yes update
+sudo apt-get --assume-yes upgrade
 sudo apt-get --assume-yes install git
-mkdir src
+sudo apt-get --assume-yes install autoconf libtool pkg-config libboost-all-dev libssl-dev libprotobuf-dev protobuf-compiler libevent-dev libqt4-dev libcanberra-gtk-module
 #git clone https://github.com/bitcoin/bitcoin.git
-cp -r ../bitcoin ./src
-cd src
-rm -rf bitcoin/src/qt/locale
-cp -R ../locale bitcoin/src/qt/
-cp ../bitcoin-config.h bitcoin/src/config/
-cp ../bitcoin.png bitcoin/src/qt/res/icons/bitcoin.png
-cp ../bitcoin.ico bitcoin/src/qt/res/icons/bitcoin.ico
+
+INSTALL_PWD_TMP=$PWD
+sudo cp -r ../bitcoin ~/
+sudo rm -rf ~/bitcoin/src/qt/locale
+sudo cp -R locale ~/bitcoin/src/qt/
+sudo cp bitcoin-config.h ~/bitcoin/src/config/
+sudo cp bitcoin.png ~/bitcoin/src/qt/res/icons/bitcoin.png
+sudo cp bitcoin.ico ~/bitcoin/src/qt/res/icons/bitcoin.ico
 sudo apt-get --assume-yes install build-essential
-wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
-tar -xvf db-4.8.30.NC.tar.gz
+sudo apt-get --assume-yes install qt5-default qttools5-dev-tools
+
+sudo mkdir ~/bitcoin/db4/
+cd ~/bitcoin/db4
+sudo wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+sudo tar -xzvf db-4.8.30.NC.tar.gz
 rm -rf db-4.8.30.NC.tar.gz
 cd db-4.8.30.NC/build_unix
-mkdir -p build
-BDB_PREFIX=$(pwd)/build
-sudo ../dist/configure --disable-shared --enable-cxx --with-pic --prefix=$BDB_PREFIX
+sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=~/bitcoin/db4/
 sudo make install
-cd ../..
-sudo apt-get --assume-yes install autoconf libtool pkg-config libboost-all-dev libssl-dev libprotobuf-dev protobuf-compiler libevent-dev libqt4-dev libcanberra-gtk-module
 
-cd bitcoin/depends
+cd ~/bitcoin/depends
 sudo update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
-cd ..
+sudo make HOST=x86_64-w64-mingw32
+cd ~/bitcoin/
+
 sudo ./autogen.sh
-CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/
-cp ../bitcoin_config.h build_msvc/bitcoin_config.h
+CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site
+sudo ./configure LDFLAGS="-L~/bitcoin/db4/lib/" CPPFLAGS="-I~/bitcoin/db4/include/"
+sudo cp $INSTALL_PWD_TMP/bitcoin_config.h ~/bitcoin/build_msvc/bitcoin_config.h
 sudo make
