ffb9f393a5800b89763cae528015766f59368526
Nikko <40539492+bitFranc@users.noreply.github.com>
2018-09-26 20:21:01 +0700

    Update chainparamsbase.cpp

--- assets_installer/bitfranc_replace/chainparamsbase.cpp
+++ assets_installer/bitfranc_replace/chainparamsbase.cpp
@@ -34,7 +34,7 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 1789);
+        return MakeUnique<CBaseChainParams>("", 1789); // RPC port for BitFranc
     else if (chain == CBaseChainParams::TESTNET)
         return MakeUnique<CBaseChainParams>("testnet3", 18332);
     else if (chain == CBaseChainParams::REGTEST)
