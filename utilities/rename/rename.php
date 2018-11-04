55c8b3f29c42da1afc426e2ab4a3c0e9fe51aa67
DESKTOP-VINJLKD\Nikko <nicolas.choukroun@yandex.com>
2018-11-04 23:29:20 +0700

    Bug fix

--- utilities/rename/rename.php
+++ utilities/rename/rename.php
@@ -21,8 +21,8 @@ replace_in_file($path_to_file."/src/chainparams.cpp","= 8333;","= 1789;");
 replace_in_file($path_to_file."/src/chainparams.cpp","18333;","11789;");
 replace_in_file($path_to_file."/src/chainparams.cpp","CreateGenesisBlock(1231006505, 2083236893, 0x1d00ffff, 1, 50 * COIN);","CreateGenesisBlock(1546300801, 981099688, 0x1d00ffff, 1, 1 * COIN);");  // genesis params
 replace_in_file($path_to_file."/src/chainparams.cpp","The Times 03/Jan/2009 Chancellor on brink of second bailout for banks","CNBC 2018/07/24 Singapore Officially Backs a CryptoCurrency and Establishes it as their Official Coin");
-replace_in_file($path_to_file."/src/chainparams.cpp","assert(consensus.hashGenesisBlock","//assert(consensus.hashGenesisBlock");
-replace_in_file($path_to_file."/src/chainparams.cpp","assert(genesis.hashMerkleRoot","//assert(genesis.hashMerkleRoot");
+//replace_in_file($path_to_file."/src/chainparams.cpp","assert(consensus.hashGenesisBlock","//assert(consensus.hashGenesisBlock");
+//replace_in_file($path_to_file."/src/chainparams.cpp","assert(genesis.hashMerkleRoot","//assert(genesis.hashMerkleRoot");
 replace_in_file($path_to_file."/src/chainparams.cpp","seed.bitcoin.sipa.be","seed1.kryptofranc.net");
 replace_in_file($path_to_file."/src/chainparams.cpp","dnsseed.bluematt.me","seed2.kryptofranc.net");
 replace_in_file($path_to_file."/src/chainparams.cpp","dnsseed.bitcoin.dashjr.org","seed3.kryptofranc.net");
@@ -52,7 +52,6 @@ replace_in_file($path_to_file."/src/chainparams.cpp","nPowTargetTimespan = 14 * 
 replace_in_file($path_to_file."/src/chainparams.cpp","04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f","41049da5cd045a7b22b11d2b4629354b040f6cc5838443c0447bcfe1d3f74025377ce3842fdb92cd1fb2041e78432b9a0a0148604303d00ccd0dfe1514d45b00d3edac"); // change the genesis public key
 replace_in_file($path_to_file."/src/chainparams.cpp","0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b","0x677ab5e51b43e828c2c227350c25258a9a74f43861759be6df1d4781a6c252cd"); // byte swapped Merkkle hash
 replace_in_file($path_to_file."/src/chainparams.cpp","0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f","0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"); // byte swapped Merkkle hash
-
 
 replace_in_file($path_to_file."/src/chainparamsbase.cpp","8332","1790"); // change RPC port value
 
