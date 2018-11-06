59290124cc4d81454541aa152cc1b91379bbca1c
DESKTOP-VINJLKD\Nikko <nicolas.choukroun@yandex.com>
2018-11-06 18:55:13 +0700

    Fix Q_INIT_RESOURCE

--- kryptofranccore/src/qt/bitcoin.cpp
+++ kryptofranccore/src/qt/bitcoin.cpp
@@ -562,8 +562,8 @@ int main(int argc, char *argv[])
     // Do not refer to data directory yet, this can be overridden by Intro::pickDataDirectory
 
     /// 1. Basic Qt initialization (not dependent on parameters or configuration)
-    Q_INIT_RESOURCE(bitcoin);
-    Q_INIT_RESOURCE(bitcoin_locale);
+    Q_INIT_RESOURCE(kryptofranc);
+    Q_INIT_RESOURCE(kryptofranc_locale);
 
     kryptoFrancApplication app(*node, argc, argv);
 #if QT_VERSION > 0x050100
