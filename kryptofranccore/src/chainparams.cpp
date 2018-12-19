5e0cec8874fd3cb044c09675ba2140e9c66f5801
DESKTOP-VINJLKD\Nikko <nicolas.choukroun@yandex.com>
2018-12-20 01:12:46 +0700

    Update chainparams.cpp

--- kryptofranccore/src/chainparams.cpp
+++ kryptofranccore/src/chainparams.cpp
@@ -155,7 +155,7 @@ public:
 
         checkpointData = {
             {
-                { 0, uint256S($hash)},
+                { 0, uint256S(0x0000000055a6f1a2fdb046a5c78e58f39f082cebcda92a6709c639e2458a23a1)},
                 //{ 33333, uint256S("0x000000002dd5588a74784eaa7ab0507a18ad16a236e7b1ce69f00d7ddfb5d0a6")},
                 //{ 74000, uint256S("0x0000000000573993a3c9e41ce34471c079dcf5f52a0e824a81e7f953b8661a20")},
                 //{ 105000, uint256S("0x00000000000291ce28027faea320c8d2b054b2e0fe44a773f3eefb151d6bdc97")},
