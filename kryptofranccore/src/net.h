19e006da97ae677974d39f79e44a7ea0ebce8ba5
Nikko <40539492+NicolasChoukroun@users.noreply.github.com>
2018-12-26 12:32:54 +0700

    Changed number of nodes to 32

--- kryptofranccore/src/net.h
+++ kryptofranccore/src/net.h
@@ -55,9 +55,9 @@ static const unsigned int MAX_PROTOCOL_MESSAGE_LENGTH = 4 * 1000 * 1000;
 /** Maximum length of strSubVer in `version` message */
 static const unsigned int MAX_SUBVERSION_LENGTH = 256;
 /** Maximum number of automatic outgoing nodes */
-static const int MAX_OUTBOUND_CONNECTIONS = 8;
+static const int MAX_OUTBOUND_CONNECTIONS = 32;
 /** Maximum number of addnode outgoing nodes */
-static const int MAX_ADDNODE_CONNECTIONS = 8;
+static const int MAX_ADDNODE_CONNECTIONS = 32;
 /** -listen default */
 static const bool DEFAULT_LISTEN = true;
 /** -upnp default */
@@ -75,7 +75,7 @@ static const unsigned int DEFAULT_MAX_PEER_CONNECTIONS = 125;
 /** The default for -maxuploadtarget. 0 = Unlimited */
 static const uint64_t DEFAULT_MAX_UPLOAD_TARGET = 0;
 /** The default timeframe for -maxuploadtarget. 1 day. */
-static const uint64_t MAX_UPLOAD_TIMEFRAME = 60 * 60 * 24;
+static const uint64_t MAX_UPLOAD_TIMEFRAME = 60 * 60 * 24; //60*60*24
 /** Default for blocks only*/
 static const bool DEFAULT_BLOCKSONLY = false;
 
@@ -84,7 +84,7 @@ static const size_t DEFAULT_MAXRECEIVEBUFFER = 5 * 1000;
 static const size_t DEFAULT_MAXSENDBUFFER    = 1 * 1000;
 
 // NOTE: When adjusting this, update rpcnet:setban's help ("24h")
-static const unsigned int DEFAULT_MISBEHAVING_BANTIME = 1 ; // Default 24-hour ban 60 * 60 * 24;
+static const unsigned int DEFAULT_MISBEHAVING_BANTIME = 60*60*24 ; // Default 24-hour ban 60 * 60 * 24;
 
 typedef int64_t NodeId;
 
