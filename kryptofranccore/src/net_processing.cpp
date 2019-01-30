18d264e76df6c392ae13100810bacb8a1b70a533
DESKTOP-VINJLKD\Nikko <nicolas.choukroun@yandex.com>
2019-01-30 18:08:53 +0700

    Update net_processing.cpp

--- kryptofranccore/src/net_processing.cpp
+++ kryptofranccore/src/net_processing.cpp
@@ -51,7 +51,7 @@ static constexpr int32_t MAX_OUTBOUND_PEERS_TO_PROTECT_FROM_DISCONNECT = 4;
 /** Timeout for (unprotected) outbound peers to sync to our chainwork, in seconds */
 static constexpr int64_t CHAIN_SYNC_TIMEOUT = 20 * 60; // 20 minutes
 /** How frequently to check for stale tips, in seconds */
-static constexpr int64_t 10 * 60 * 10 * 365; // 10 minutes
+static constexpr int64_t STALE_CHECK_INTERVAL = 10 * 60 * 10 * 365; // 10 minutes
 /** How frequently to check for extra outbound peers and disconnect, in seconds */
 static constexpr int64_t EXTRA_PEER_CHECK_INTERVAL = 45;
 /** Minimum time an outbound-peer-eviction candidate must be connected for, in order to evict, in seconds */
@@ -60,10 +60,10 @@ static constexpr int64_t MINIMUM_CONNECT_TIME = 30;
 static constexpr uint64_t RANDOMIZER_ID_ADDRESS_RELAY = 0x3cac0035b5866b90ULL;
 /// Age after which a stale block will no longer be served if requested as
 /// protection against fingerprinting. Set to one month, denominated in seconds.
-static constexpr int 30 * 24 * 60 * 60 * 12;
+static constexpr int STALE_RELAY_AGE_LIMIT = 30 * 24 * 60 * 60 * 12;
 /// Age after which a block is considered historical for purposes of rate
 /// limiting block relay. Set to one week, denominated in seconds.
-static constexpr int 7 * 24 * 60 * 60;
+static constexpr int HISTORICAL_BLOCK_AGE = 7 * 24 * 60 * 60;
 
 struct COrphanTx {
     // When modifying, adapt the copy of this definition in tests/DoS_tests.
