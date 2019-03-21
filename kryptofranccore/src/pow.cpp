75bb02f0b312d5cca12f27799bbd44e7230c1643
Nikko <40539492+NicolasChoukroun@users.noreply.github.com>
2019-03-22 00:01:14 +0700

    Update pow.cpp

--- kryptofranccore/src/pow.cpp
+++ kryptofranccore/src/pow.cpp
@@ -12,92 +12,6 @@
 
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
-    return DarkGravityWave3(pindexLast, pblock, params);
-}
-
-// from https://gist.github.com/GeertJohan/b28da8105babf0553f21
-unsigned int DarkGravityWave3(const CBlockIndex* pindexLast, const CBlockHeader *pblock,const Consensus::Params& params) {
-    /* current difficulty formula, darkcoin - DarkGravity v3, written by Evan Duffield - evan@darkcoin.io */
-    const CBlockIndex *BlockLastSolved = pindexLast;
-    const CBlockIndex *BlockReading = pindexLast;
-    const CBlockHeader *BlockCreating = pblock;
-    BlockCreating = BlockCreating;
-    int64_t bnProofOfWorkLimit=0x1e0fffff;
-    int64_t nActualTimespan = 0;
-    int64_t LastBlockTime = 0;
-    int64_t PastBlocksMin = 24;
-    int64_t PastBlocksMax = 24;
-    int64_t CountBlocks = 0;
-    arith_uint256 PastDifficultyAverage;
-    arith_uint256 PastDifficultyAveragePrev;
-
-    if (BlockLastSolved == NULL || BlockLastSolved->nHeight == 0 || BlockLastSolved->nHeight < PastBlocksMin) {
-        // This is the first block or the height is < PastBlocksMin
-        // Return minimal required work. (1e0fffff)
-        return bnProofOfWorkLimit;
-    }
-
-    // loop over the past n blocks, where n == PastBlocksMax
-    for (unsigned int i = 1; BlockReading && BlockReading->nHeight > 0; i++) {
-        if (PastBlocksMax > 0 && i > PastBlocksMax) { break; }
-        CountBlocks++;
-
-        // Calculate average difficulty based on the blocks we iterate over in this for loop
-        if(CountBlocks <= PastBlocksMin) {
-            if (CountBlocks == 1) { PastDifficultyAverage.SetCompact(BlockReading->nBits); }
-            else { PastDifficultyAverage = ((PastDifficultyAveragePrev * CountBlocks)+(BlockReading->nBits)) / (CountBlocks+1); }
-            PastDifficultyAveragePrev = PastDifficultyAverage;
-        }
-
-        // If this is the second iteration (LastBlockTime was set)
-        if(LastBlockTime > 0){
-            // Calculate time difference between previous block and current block
-            int64_t Diff = (LastBlockTime - BlockReading->GetBlockTime());
-            // Increment the actual timespan
-            nActualTimespan += Diff;
-        }
-        // Set LasBlockTime to the block time for the block in current iteration
-        LastBlockTime = BlockReading->GetBlockTime();
-
-        if (BlockReading->pprev == NULL) { assert(BlockReading); break; }
-        BlockReading = BlockReading->pprev;
-    }
-
-    // bnNew is the difficulty
-    arith_uint256 bnNew(PastDifficultyAverage);
-
-    // nTargetTimespan is the time that the CountBlocks should have taken to be generated.
-    int64_t nTargetTimespan = CountBlocks*params.nPowTargetSpacing;
-
-    // Limit the re-adjustment to 5x or 0.33x
-
-    if (nActualTimespan < nTargetTimespan/3)
-        nActualTimespan = nTargetTimespan/3;
-    if (nActualTimespan > nTargetTimespan*5)
-        nActualTimespan = nTargetTimespan*5;
-
-    // Calculate the new difficulty based on actual and target timespan.
-    bnNew *= nActualTimespan;
-    bnNew /= nTargetTimespan;
-
-    // If calculated difficulty is lower than the minimal diff, set the new difficulty to be the minimal diff.
-    if (bnNew > bnProofOfWorkLimit){
-        bnNew = bnProofOfWorkLimit;
-    }
-
-    // Some logging.
-    // TODO: only display these log messages for a certain debug option.
-    printf("Difficulty Retarget - Dark Gravity Wave 3\n");
-    printf("Before: %02x\n", BlockLastSolved->nBits);
-    printf("After: %02x\n", bnNew.GetCompact());
-
-    // Return the new diff.
-    return bnNew.GetCompact();
-}
-
-
-/*unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
-{
     assert(pindexLast != nullptr);
     unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
 
@@ -131,17 +45,14 @@ unsigned int DarkGravityWave3(const CBlockIndex* pindexLast, const CBlockHeader 
 
     return CalculateNextWorkRequired(pindexLast, pindexFirst->GetBlockTime(), params);
 }
-*/
+
 unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params& params)
 {
-    // nikko do not retarget when < 10000 for premine
-    //if (params.fPowNoRetargeting || pindexLast->nHeight<10000) return pindexLast->nBits;
-	if (params.fPowNoRetargeting)
+    if (params.fPowNoRetargeting)
         return pindexLast->nBits;
 
     // Limit adjustment step
     int64_t nActualTimespan = pindexLast->GetBlockTime() - nFirstBlockTime;
-
     if (nActualTimespan < params.nPowTargetTimespan/4)
         nActualTimespan = params.nPowTargetTimespan/4;
     if (nActualTimespan > params.nPowTargetTimespan*4)
@@ -165,39 +76,16 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
     bool fNegative;
     bool fOverflow;
     arith_uint256 bnTarget;
-    printf("CheckProofOfWork: nBits = %i.\n",nBits);
 
     bnTarget.SetCompact(nBits, &fNegative, &fOverflow);
 
     // Check range
-    if (fNegative ) {
-        printf("CheckProofOfWork: range not good exit to false: fNegative=true.\n");
+    if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
         return false;
-    }
-
-    if (fOverflow ) {
-        printf("CheckProofOfWork: range not good exit to false: fOverflow=true.\n");
-        return false;
-    }
-    if (bnTarget > UintToArith256(params.powLimit)) {
-        printf("CheckProofOfWork: range not good exit to false. %s > %s \n",bnTarget.GetHex().c_str(),UintToArith256(params.powLimit).GetHex().c_str());
-        return false;
-    }
-	if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit)) {
-        printf("CheckProofOfWork: bnTarget=0");
-        return false;
-    }
-
-    if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit)) {
-        printf("CheckProofOfWork: range not good exit to false.");
-        return false;
-    }
 
     // Check proof of work matches claimed amount
-    if (UintToArith256(hash) > bnTarget) {
-        printf("CheckProofOfWork: do not match claimed. bnTarget=%s - hash=%s",bnTarget.GetHex().c_str(),UintToArith256(hash).GetHex().c_str());
+    if (UintToArith256(hash) > bnTarget)
         return false;
-    }
 
     return true;
 }
